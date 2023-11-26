675a0a2dbd136cc9931513dfc4f99489cd311507
Pedro Sanchez <pedrosava01@gmail.com>
2023-11-26 10:22:30 -0600

    act

---
+++ hydroprotect/components/sensor.jsx
@@ -0,0 +1,8 @@
+
+const Sensor = () => {
+  return (
+    <div className=" uppercase text-center bg-gray-700">Sensor</div>
+  )
+}
+
+export default Sensor
---
+++ hydroprotect/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Sensor from '../components/sensor'
+
+function App() {
+  
+
+  return (
+    <>
+      <Sensor/>
+    </>
+  )
+}
+
+export default App
